36e1f140a5f603e4de34baa99ecd1e2b555c1d0e
Eric Brelsford <ebrelsford@gmail.com>
2013-10-04 13:04:20 -0400

    AddGenericMixin

--- livinglots_genericviews/views.py
+++ livinglots_genericviews/views.py
@@ -1,8 +1,10 @@
 import csv
 
+from django.contrib.contenttypes.models import ContentType
 from django.core.serializers import json
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.views.generic import View
+from django.views.generic.edit import FormMixin
 
 
 class CSVView(View):
@@ -65,3 +67,48 @@ class JSONResponseView(View):
                             cls=json.DjangoJSONEncoder, ensure_ascii=False),
             mimetype='application/json',
         )
+
+
+class AddGenericMixin(FormMixin):
+    """
+    A mixin that eases adding content that references a model instance using a
+    generic relationship. It adds the content_type and object_id for the
+    relationship to the view's form's initial data.
+    """
+    content_type_model = None
+    content_type_id_key = 'content_type_id'
+    object_id_key = 'pk'
+
+    def get_object_id_key(self):
+        return self.object_id_key
+
+    def get_content_type_id_key(self):
+        return self.content_type_id_key
+
+    def get_content_object(self):
+        return self.get_content_type().get_object_for_this_type(
+            pk=self.get_content_object_id()
+        )
+
+    def get_content_object_id(self):
+        return self.kwargs[self.get_object_id_key()]
+
+    def get_content_type(self):
+        if self.content_type_model:
+            return ContentType.objects.get_for_model(self.content_type_model)
+        content_type_id = self.kwargs[self.get_content_type_id_key()]
+        return ContentType.objects.get_for_id(content_type_id)
+
+    def get_initial(self):
+        """Add initial content_type and object_id to the form"""
+        initial = super(AddGenericMixin, self).get_initial()
+        try:
+            content_type = self.get_content_type()
+            object_id = self.kwargs[self.get_object_id_key()]
+        except KeyError:
+            raise Http404
+        initial.update({
+            'content_type': content_type,
+            'object_id': object_id,
+        })
+        return initial
